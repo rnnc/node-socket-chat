8b27d57f46debed221c379d48e13349002af11c6
rnnc <rnnc@github.com>
2019-03-09 17:11:20 -0500

    layed out initial styling of chat

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,22 +6,37 @@ class App extends Component {
     super();
     this.state = {
       username: null,
-      messages: ['hello', 'how', 'are', 'you']
+      messages: ['hello', 'how', 'are', 'you'],
+      currentMsg: null,
     }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
   componentDidMount() {
-    const username = prompt('Enter username');
+    //const username = prompt('Enter username');
+    const username = 'rohan';
     this.setState({ username });
   }
 
-  _handleKeyPress(e) {
-    console.log(e.key);
+  handleChange(e) {
+
+    if (e.key === 'Enter') {
+      console.log('enter');
+      this.addNewMessages(e.target.value);
+    }
   }
 
-  messagesToComponents(messages) {
-    return messages.map(msg => (
-      <li>{ msg }</li>
-    ))
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState({ currentMsg: e.target.Value });
+
+  }
+
+  addNewMessages(message) {
+    console.log(message);
+    const nMessages = [...this.state.messages, message];
+    this.setState({ messages: nMessages });
   }
 
   render() {
@@ -35,13 +50,20 @@ class App extends Component {
         </div>
         <div className="chat">
           <ul className="messages">
-            { messages.map(msg => (
-              <li>{ msg }</li>
+            { messages.map((msg, i) => (
+              <li key={ i }>{ msg }</li>
             )) }
           </ul>
         </div>
-        <div className="message-box">
-          <input type="text" onKeyPress={ this._handleKeyPress } />
+        <div className="composer">
+          <form onSubmit={ this.handleSubmit } className="message-form">
+            <input type="text" maxLength={ 250 }
+              autoComplete={ false }
+              value={ this.state.currentMsg }
+              onChange={ this.handleChange }
+              placeholder="Type a message..."
+            />
+          </form>
         </div>
       </div>
     );
